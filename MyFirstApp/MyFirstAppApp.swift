11d103f25ad52e29a2472df3ad449fd1adcd74c9
Ned Ememanka <officialchinedu@gmail.com>
2021-07-11 23:25:13 -0400

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Ned Ememanka on 2021-07-11.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
